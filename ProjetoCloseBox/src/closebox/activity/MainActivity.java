34bf74c24c24aa9c11fda8f484094b2324eb3700
reinaldomakinista@gmail.com <reinaldomakinista@gmail.com@bb3fd425-7e22-b127-c025-ea63b794f136>
2012-08-22 19:52:30 +0000

    inserido comentarios

--- ProjetoCloseBox/src/closebox/activity/MainActivity.java
+++ ProjetoCloseBox/src/closebox/activity/MainActivity.java
@@ -11,36 +11,40 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import closebox.model.*;
 
+/**
+ * Classe responsavel por mostrar a tela inicial do jogo e de acordo com o botao tocado pelo jogador, inicializa uma nova Activity
+ * @author Reinaldo
+ *
+ */
 public class MainActivity extends Activity {
     
     private Intent intent;
     
     public void botaoInicio(View view){
-    	intent = new Intent(MainActivity.this, ControllerActivity.class);
-    	intent.putExtra("botao", "inicio");
-    	startActivity(intent);
+    	intent = new Intent(MainActivity.this, ControllerActivity.class); //determina a nova Activity
+    	intent.putExtra("botao", "inicio"); // o nome do botao, na verdade uma referencia a ser tratada no controller
+    	startActivity(intent); // inicializa a nova Activity, envia os dados ao controller
 		
     }
     
     public void botaoOptions(View view){
-    	intent = new Intent(MainActivity.this, ControllerActivity.class);
-    	intent.putExtra("botao", "options");
-		startActivity(intent);
+    	intent = new Intent(MainActivity.this, ControllerActivity.class); //determina a nova Activity
+    	intent.putExtra("botao", "options"); // o nome do botao, na verdade uma referencia a ser tratada no controller
+		startActivity(intent); // inicializa a nova Activity, envia os dados ao controller
 		
     }
     
     public void botaoScore(View view){
-    	intent = new Intent(MainActivity.this, ControllerActivity.class);
-    	intent.putExtra("botao", "score");
-		startActivity(intent);
+    	intent = new Intent(MainActivity.this, ControllerActivity.class); //determina a nova Activity
+    	intent.putExtra("botao", "score"); // o nome do botao, na verdade uma referencia a ser tratada no controller
+		startActivity(intent); // inicializa a nova Activity, envia os dados ao controller
 		
     }
     
     public void botaoSobre(View view){
-    	intent = new Intent(MainActivity.this, ControllerActivity.class);
-    	intent.putExtra("botao", "sobre");
-    	//dialogo2(intent.get);
-		startActivity(intent);
+    	intent = new Intent(MainActivity.this, ControllerActivity.class); //determina a nova Activity
+    	intent.putExtra("botao", "sobre"); // o nome do botao, na verdade uma referencia a ser tratada no controller
+    	startActivity(intent); // inicializa a nova Activity, envia os dados ao controller
     }
     
     
