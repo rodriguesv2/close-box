c3f750796ddd0fe70916b122d5fb656a6a5e7d17
rubensrodriguesv2@gmail.com <rubensrodriguesv2@gmail.com@bb3fd425-7e22-b127-c025-ea63b794f136>
2012-09-08 05:05:17 +0000

    Inclusão da musica tema (provisoria), e de classes e pacote necessários para musica e fx (não concluido)

---
+++ trunk/ProjetoCloseBox/src/closebox/service/MusicaPrincipalService.java
@@ -0,0 +1,44 @@
+package closebox.service;
+
+import closebox.activity.R;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class MusicaPrincipalService extends Service{
+	
+	private MediaPlayer mediaPlayer;
+    private IBinder myBinder = new LocalBinder();
+    
+    public class LocalBinder extends Binder {
+        public MusicaPrincipalService getService() {
+            return MusicaPrincipalService.this;
+        }
+    }
+    
+    public void pauseMusic(){
+    	mediaPlayer.pause();
+    }
+    
+    public void playMusic(){
+    	mediaPlayer.start();
+    }
+    
+    @Override  
+    public IBinder onBind(Intent i) {
+        return myBinder;  
+    }  
+    @Override  
+    public void onCreate() {  
+    		mediaPlayer = MediaPlayer.create(this, R.raw.yoho);
+    		mediaPlayer.start();
+    }  
+      
+    @Override  
+    public void onDestroy() {  
+    	mediaPlayer.stop();
+    	mediaPlayer.release();
+    }
+}
