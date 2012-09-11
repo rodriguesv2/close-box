07315d290dc80de007f8ca27110eedb42dce1352
rubensrodriguesv2@gmail.com <rubensrodriguesv2@gmail.com@bb3fd425-7e22-b127-c025-ea63b794f136>
2012-09-11 03:28:02 +0000

    Começo do desenvolvimento da ultima tela do jogo (opções), ainda não funcionando.

--- trunk/ProjetoCloseBox/src/closebox/activity/OptionsActivity.java
+++ trunk/ProjetoCloseBox/src/closebox/activity/OptionsActivity.java
@@ -1,7 +1,15 @@
 package closebox.activity;
 
+import closebox.service.MusicaPrincipalService;
+import closebox.service.MusicaPrincipalService.LocalBinder;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.CheckBox;
 import android.widget.TextView;
 /**
  * PROVISORIO
@@ -11,11 +19,64 @@ import android.widget.TextView;
 public class OptionsActivity extends Activity{
 	
 	private TextView credito;
+	private CheckBox musicaCheck;
+	private CheckBox somCheck;
+	private boolean booleanMusicaCheck;
+	private boolean booleanSomCheck;
+	private boolean mBound = false;
+	private MusicaPrincipalService musicaPrincipalService;
+	private ServiceConnection serviceConnection = new ServiceConnection() {
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			mBound = false;
+			
+		}
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			LocalBinder localBinder = (LocalBinder)service;
+			musicaPrincipalService = localBinder.getService();
+			musicaPrincipalService.playMusic();
+			mBound = true;
+		}
+	};
 	
 	@Override 
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.opcoes);
 		
+		musicaCheck = (CheckBox)findViewById(R.id.checkMusica);
+		somCheck = (CheckBox)findViewById(R.id.checkSom);
+		
+		bindService(new Intent(this, MusicaPrincipalService.class), serviceConnection, Context.BIND_AUTO_CREATE);
 	}
+	
+	@Override
+	public void onResume(){
+		if(mBound)
+			musicaPrincipalService.playMusic();
+		super.onResume();
+	}
+	
+	@Override
+	public void onPause(){
+		if(mBound)
+			musicaPrincipalService.pauseMusic();
+		super.onPause();
+	}
+	
+	@Override
+	public void onStart(){
+		if(mBound)
+			musicaPrincipalService.playMusic();
+		super.onStart();
+	}
+	
+	@Override
+	public void onDestroy(){
+		if(mBound)
+			unbindService(serviceConnection);
+		super.onDestroy();
+	}
+	
 }
